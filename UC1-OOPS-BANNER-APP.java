7eb7d63a9db8a63ddd8ba96cca781012cdcaaed1
ks8977-cyber <ks8977@srmist.edu.in>
2026-02-23 22:20:48 +0530

    Update UC1-OOPS-BANNER-APP.java

--- UC1-OOPS-BANNER-APP.java
+++ UC1-OOPS-BANNER-APP.java
@@ -1,6 +1,5 @@
 public class OopsBanner {
     public static void main(String[] args) {
-        // UC-1: Simple hardcoded banner output for "OOPS"
         System.out.println(" *** *** **** ****");
         System.out.println("* * * * * * * ");
         System.out.println("* * * * **** *** ");
